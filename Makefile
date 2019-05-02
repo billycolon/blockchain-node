aeternity/aeternity

commit 2eb2f61df6fa22298ec90160a48172eb9ef369d7

Add eunit rebar flags to Make target

Makefile
@@ -6,6 +6,7 @@ REBAR ?= ./rebar3
 EUNIT_VM_ARGS = $(CURDIR)/config/eunit.vm.args
 EUNIT_SYS_CONFIG = $(CURDIR)/config/eunit.sys.config
 EUNIT_TEST_FLAGS ?=
+EUNIT_REBAR_FLAGS ?=
 
 CT_TEST_FLAGS ?=
 ST_CT_FLAGS = --logdir system_test/logs
@@ -209,7 +210,7 @@ REVISION:
 
 eunit: KIND=test
 eunit: internal-build
-	@ERL_FLAGS="-args_file $(EUNIT_VM_ARGS) -config $(EUNIT_SYS_CONFIG)" ./rebar3 do eunit $(EUNIT_TEST_FLAGS)
+	@ERL_FLAGS="-args_file $(EUNIT_VM_ARGS) -config $(EUNIT_SYS_CONFIG)" ./rebar3 do eunit $(EUNIT_REBAR_FLAGS) $(EUNIT_TEST_FLAGS)
 
 eunit-roma: KIND=test
 eunit-roma: internal-build

billycolon/blockchain-node

commit a06e3f3b4c6a6b578513b93b8e44747863d8590a

Delete test flag repeated by mistake
Introduced in https://github.com/aeternity/aeternity/commit/2eb2f61df

Deleted in https://github.com/aeternity/aeternity/commit/e5a0b3c15#diff-b67911656ef5d18c4ae36cb6741b7965R223

This commit removes leftover.

Makefile
@@ -6,7 +6,6 @@ REBAR ?= ./rebar3
 EUNIT_VM_ARGS = $(CURDIR)/config/eunit.vm.args
 EUNIT_SYS_CONFIG = $(CURDIR)/config/eunit.sys.config
 EUNIT_TEST_FLAGS ?=
-EUNIT_REBAR_FLAGS ?=
 
 CT_TEST_FLAGS ?=
 ST_CT_FLAGS = --logdir system_test/logs
@@ -228,7 +227,7 @@ REVISION:
 
 eunit: KIND=test
 eunit: internal-build
-	@ERL_FLAGS="-args_file $(EUNIT_VM_ARGS) -config $(EUNIT_SYS_CONFIG)" $(REBAR) do eunit $(EUNIT_TEST_FLAGS) $(EUNIT_TEST_FLAGS)
+	@ERL_FLAGS="-args_file $(EUNIT_VM_ARGS) -config $(EUNIT_SYS_CONFIG)" $(REBAR) do eunit $(EUNIT_TEST_FLAGS)
 
 eunit-roma: KIND=test
 eunit-roma: internal-build